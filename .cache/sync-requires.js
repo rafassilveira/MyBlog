3e57eb7c90708360799bbde7dd0ed5ab6268fcaa
Rafael Silveira <rafaelssilveira@outlook.com>
2019-12-07 21:43:09 -0300

    teste

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("D:\\Git\\my-blog\\node_modules\\gatsby-plugin-offline\\app-shell.js"))),
   "component---src-templates-blog-post-js": hot(preferDefault(require("D:\\Git\\my-blog\\src\\templates\\blog-post.js"))),
   "component---src-templates-blog-list-js": hot(preferDefault(require("D:\\Git\\my-blog\\src\\templates\\blog-list.js"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("D:\\Git\\my-blog\\.cache\\dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("D:\\Git\\my-blog\\src\\pages\\404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("D:\\Git\\my-blog\\src\\pages\\about.js"))),
   "component---src-pages-search-js": hot(preferDefault(require("D:\\Git\\my-blog\\src\\pages\\search.js")))
